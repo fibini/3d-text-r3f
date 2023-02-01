426e10ffb99acca22a83bd8010f6edf22889b0b8
fibini <fabienswift@gmail.com>
2023-02-01 13:05:57 -0400

    Add random capsules to the scene

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,16 +1,20 @@
 import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { TorusGeometry } from 'three'
+import { useState } from 'react'
 
 export default function Experience()
 {
     const [ matcapTexture ] = useMatcapTexture('070B0C_B2C7CE_728FA3_5B748B', 256)
+
+    const [ capsuleGeometry, setCapsuleGeometry ] = useState()
     
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
+
+        <capsuleGeometry ref={ setCapsuleGeometry }/>
 
         {/* <mesh scale={ 1.5 }>
             <boxGeometry />
@@ -32,6 +36,27 @@ export default function Experience()
                 <meshMatcapMaterial matcap={ matcapTexture } />
             </Text3D>
         </Center>
+        {[...Array(100)].map((value, index) =>
+            <mesh
+                key={ index }
+                geometry={ capsuleGeometry }
+                position={ [
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ] }
+                scale={ 0.3 + Math.random() * 0.3 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
+            
+            >
+                <meshMatcapMaterial matcap={ matcapTexture } />
+            </mesh>
+        )}
+        
 
     </>
 }
